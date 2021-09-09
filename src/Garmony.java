506edc9031343790a2750438756643abcc606fcf
CRYZORE <cryzore@gmail.com>
2021-09-09 16:44:26 +0300

    Initial commit

---
+++ src/Garmony.java
@@ -0,0 +1,10 @@
+public class Garmony {
+    public static void main(String[] args) {
+        float Sum =0;
+        for(float i=1;i<=10;i++){
+            System.out.println(1/i);
+            Sum=Sum+1/i;
+        }
+        System.out.println("Сумма = "+Sum);
+    }
+}
